55b3f1b4cec1dfb1d842eb08c098af788e225e58
LiGreT <ligret.regret@gmail.com>
2025-09-30 11:29:43 +0900

    PlayerManagerContextをPlayerManagerに変更し、出力結果の処理を修正

--- ResultOutput.py
+++ ResultOutput.py
@@ -1,23 +1,23 @@
 from abc import ABC, abstractmethod
-from PlayerManager import PlayerManagerContext
+from PlayerManager import PlayerManager
 from MessageOutput import MessageOutputContext, MessageOutputToTerminal
 
 class ResultOutput(ABC):
     @abstractmethod
 
-    def output_result(self, result, player_manager: PlayerManagerContext):
+    def output_result(self, result, player_manager: PlayerManager):
         pass
 
 class ResultOutputToTerminal(ResultOutput):
-    def output_result(self, result, player_manager_context: PlayerManagerContext):
+    def output_result(self, result, player_manager: PlayerManager):
         message_output_context = MessageOutputContext()
         message_output_context.set_message_output(MessageOutputToTerminal())
 
         # 出力文字列設定
         if(result == 0):
-            message_output_context.execute_output_message(f"{player_manager_context.player_manager.first_player.name}さんの勝利です。")
+            message_output_context.execute_output_message(f"{player_manager.first_player.name}さんの勝利です。")
         elif(result == 1):
-            message_output_context.execute_output_message(f"{player_manager_context.player_manager.second_player.name}さんの勝利です。")
+            message_output_context.execute_output_message(f"{player_manager.second_player.name}さんの勝利です。")
         elif(result == 2):
             message_output_context.execute_output_message("引き分けです。")
         else:
@@ -30,8 +30,8 @@ class ResultOutputContext:
     def set_method(self, result_output: ResultOutput):
         self.result_output = result_output
     
-    def execute_output(self, result, player_manager_context: PlayerManagerContext):
+    def execute_output(self, result, player_manager: PlayerManager):
         if self.result_output is not None:
-            self.result_output.output_result(result, player_manager_context)
+            self.result_output.output_result(result, player_manager)
         else:
             print("No method set up")
