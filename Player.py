4377a14bf3f552b3e55dc9e431258c947386ab5d
LiGreT <ligret.regret@gmail.com>
2025-10-02 15:59:27 +0900

    ランダムCOMとテスト実装

--- Player.py
+++ Player.py
@@ -3,6 +3,7 @@ from Board import Board
 from Processing import Processing
 from MessageOutput import MessageOutputContext, MessageOutputToTerminal
 from time import sleep
+import random
 
 class Player(ABC):
     @abstractmethod
@@ -38,6 +39,33 @@ class HumanPlayerFromTerminal(Player):
             else:
                 message_output_context.execute_output_message("そこには置けません。")
             sleep(0.2)
+        return board
+    
+class RandomComputerPlayerFromTerminal(Player):
+    def __init__(self, color, name):
+        super().__init__(color, name)
+    
+    def put(self, board: Board) -> Board:
+        processing = Processing()
+        message_output_context = MessageOutputContext()
+        message_output_context.set_message_output(MessageOutputToTerminal)
+
+        processing.find_putable(self.color, board)
+        l = len(processing.putable_coordinates)
+        if(l == 0):
+            message_output_context.execute_output_message("置ける場所がありません。")
+
+        random.seed()
+        select = random.randrange(l)
+        for _ in range(select+1): place = processing.putable_coordinates.pop()
+        x, y = place[0], place[1]
+        processing.find_flippable(x, y, self.color, board)
+        if(processing.is_valid_put()):
+            board = processing.put(x, y, self.color, board)
+            board = processing.flip(board)
+        else:
+            message_output_context.execute_output_message("そこには置けません。")
+
         return board
 
 class PlayerContext:
--- Player_test.py
+++ Player_test.py
@@ -1,4 +1,4 @@
-from Player import PlayerContext, HumanPlayerFromTerminal
+from Player import PlayerContext, HumanPlayerFromTerminal, RandomComputerPlayerFromTerminal
 from Board import Board, BoardOutputContext, BoardOutputToTerminal
 
 class TestPlayer:
@@ -9,11 +9,19 @@ class TestPlayer:
 
         board_output_context.set_method(BoardOutputToTerminal())
         board_output_context.execute_output_board(board)
-        
-        player_context.set_method(HumanPlayerFromTerminal(0, 'White'))
-        player_context.execute_put(board)
 
-        board_output_context.execute_output_board(board)
+        instance_dict = {
+            0 : HumanPlayerFromTerminal(0, "White"),
+            1 : RandomComputerPlayerFromTerminal(0, "White")
+        }
+        
+        key = int(input("key : "))
+        if(key in instance_dict.keys()):
+            player_context.set_method(instance_dict[key])
+            player_context.execute_put(board)
+            board_output_context.execute_output_board(board)
+        else:
+            print("keyが不正です。")
 
 if __name__ == '__main__':
     test_player = TestPlayer()
