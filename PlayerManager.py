b45a94e1edd1a8494351bdf12c3d5b625483e3c9
LiGreT <ligret.regret@gmail.com>
2025-10-04 10:00:00 +0900

    PlayerManagerの構造を変更

--- PlayerManager.py
+++ PlayerManager.py
@@ -1,56 +1,24 @@
-from abc import ABC, abstractmethod
-from Player import HumanPlayerFromTerminal
+from Player import HumanPlayerFromTerminal, RandomComputerPlayerFromTerminal
 
-class PlayerManager(ABC):
-    @abstractmethod
+class PlayerManager:
+    HUMAN_T = 0
+    RCOM_T = 1
+
+    player_dict = {
+        HUMAN_T : HumanPlayerFromTerminal,
+        RCOM_T : RandomComputerPlayerFromTerminal
+    }
+
     def __init__(self):
         self.first_player = None
         self.second_player = None
     
-    def register_first_player(self, name, color):
-        pass
+    def register_first_player(self, color, name, key):
+        self.first_player = PlayerManager.player_dict[key](color, name)
 
-    def register_second_player(self, name, color):
-        pass
-
-    def switch(self):
-        pass
-
-class PlayerManagerForHumanVsHuman(PlayerManager):
-    def __init__(self):
-        self.first_player = None
-        self.second_player = None
-    
-    def register_first_player(self, color, name):
-        self.first_player = HumanPlayerFromTerminal(color, name)
-
-    def register_second_player(self, color, name):
-        self.second_player = HumanPlayerFromTerminal(color, name)
+    def register_second_player(self, color, name, key):
+        self.second_player = PlayerManager.player_dict[key](color, name)
     
     def switch(self):
-        self.first_player, self.second_player = self.second_player, self.first_player
-    
-class PlayerManagerContext:
-    def __init__(self):
-        self.player_manager = None
-
-    def set_method(self, player_manager: PlayerManager):
-        self.player_manager = player_manager
-    
-    def execute_register_first_player(self, color, name):
-        if self.player_manager is not None:
-            self.player_manager.register_first_player(color, name)
-        else:
-            print("No method set up")
-    
-    def execute_register_second_player(self, color, name):
-        if self.player_manager is not None:
-            self.player_manager.register_second_player(color, name)
-        else:
-            print("No method set up")
-    
-    def execute_switch(self):
-        if self.player_manager is not None:
-            self.player_manager.switch()
-        else:
-            print("No method set up")
+        if(self.first_player != None and self.second_player != None):
+            self.first_player, self.second_player = self.second_player, self.first_player
