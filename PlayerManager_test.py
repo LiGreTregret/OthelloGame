1edc481600735e3b5339313ab146b4412e2cef51
LiGreT <ligret.regret@gmail.com>
2025-10-04 10:08:15 +0900

    test_switch追加

--- PlayerManager_test.py
+++ PlayerManager_test.py
@@ -12,3 +12,12 @@ class TestPlayerManager:
         player_manager.register_second_player(0, "White", PM.HUMAN_T)
         assert player_manager.second_player.color == 0
         assert player_manager.second_player.name == "White"
+    
+    def test_switch(self):
+        player_manager = PM()
+        player_manager.register_first_player(0, "White", PM.HUMAN_T)
+        player_manager.register_second_player(1, "Black", PM.HUMAN_T)
+        assert player_manager.first_player.color == 0
+        assert player_manager.first_player.name == "White"
+        assert player_manager.second_player.color == 1
+        assert player_manager.second_player.name == "Black"
