639d1866f64d731cce8fbf6d04f6b9c248b019e4
LiGreT <ligret.regret@gmail.com>
2025-10-04 10:27:44 +0900

    PM修正atGM_test

--- GameMode_test.py
+++ GameMode_test.py
@@ -1,17 +1,16 @@
-from PlayerManager import PlayerManagerContext, PlayerManagerForHumanVsHuman
+from PlayerManager import PlayerManager as PM
 from GameMode import GameModeContext, TerminalMode
 
 class TestGameMode:
     def t_game(self):
-        player_manager_context = PlayerManagerContext()
+        player_manager= PM()
         game_mode_context = GameModeContext()
         
-        player_manager_context.set_method(PlayerManagerForHumanVsHuman())
-        player_manager_context.execute_register_first_player(0, "White")
-        player_manager_context.execute_register_second_player(1, "Black")
+        player_manager.register_first_player(0, "White", PM.RCOM_T)
+        player_manager.register_second_player(1, "Black", PM.RCOM_T)
 
         game_mode_context.set_mode(TerminalMode())
-        game_mode_context.execute_game(player_manager_context.player_manager)
+        game_mode_context.execute_game(player_manager)
 
 if __name__ == "__main__":
     test_game_mode = TestGameMode()
