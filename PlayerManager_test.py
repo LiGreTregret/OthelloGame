341ac891a81c1dfda29be52923337d797d620c11
LiGreT <ligret.regret@gmail.com>
2025-10-04 10:05:03 +0900

    テスト修正

--- PlayerManager_test.py
+++ PlayerManager_test.py
@@ -1,16 +1,14 @@
-from PlayerManager import PlayerManagerContext, PlayerManagerForHumanVsHuman
+from PlayerManager import PlayerManager as PM
 
 class TestPlayerManager:
     def test_register_first_player(self):
-        player_manager_context = PlayerManagerContext()
-        player_manager_context.set_method(PlayerManagerForHumanVsHuman())
-        player_manager_context.execute_register_first_player(0, "White")
-        assert player_manager_context.player_manager.first_player.color == 0
-        assert player_manager_context.player_manager.first_player.name == "White"
+        player_manager = PM()
+        player_manager.register_first_player(0, "White", PM.HUMAN_T)
+        assert player_manager.first_player.color == 0
+        assert player_manager.first_player.name == "White"
         
     def test_register_second_player(self):
-        player_manager_context = PlayerManagerContext()
-        player_manager_context.set_method(PlayerManagerForHumanVsHuman())
-        player_manager_context.execute_register_second_player(1, "Black")
-        assert player_manager_context.player_manager.second_player.color == 1
-        assert player_manager_context.player_manager.second_player.name == "Black"
+        player_manager = PM()
+        player_manager.register_second_player(0, "White", PM.HUMAN_T)
+        assert player_manager.second_player.color == 0
+        assert player_manager.second_player.name == "White"
