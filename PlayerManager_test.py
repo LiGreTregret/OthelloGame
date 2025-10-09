55bdbc267cd9f1edb45219014586b558b14dbd17
LiGreT <ligret.regret@gmail.com>
2025-10-09 14:59:08 +0900

    PlayerManager_testにもForTerminalの追加

--- PlayerManager_test.py
+++ PlayerManager_test.py
@@ -1,14 +1,14 @@
-from PlayerManager import PlayerManager as PM
+from PlayerManager import PlayerManagerForTerminal as PMT
 
 class TestPlayerManager:
     def test_register_first_player(self):
-        player_manager = PM()
-        player_manager.register_first_player(0, "White", PM.HUMAN_T)
+        player_manager = PMT()
+        player_manager.register_first_player(0, "White", PMT.HUMAN_T)
         assert player_manager.first_player.color == 0
         assert player_manager.first_player.name == "White"
         
     def test_register_second_player(self):
-        player_manager = PM()
-        player_manager.register_second_player(0, "White", PM.HUMAN_T)
+        player_manager = PMT()
+        player_manager.register_second_player(0, "White", PMT.HUMAN_T)
         assert player_manager.second_player.color == 0
         assert player_manager.second_player.name == "White"
