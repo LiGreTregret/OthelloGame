4f9ebdabc49df5ebdd66dfc08e26dd6a14b1b3a6
LiGreT <ligret.regret@gmail.com>
2025-10-19 08:49:57 +0900

    GUIPlayerInputDesignForHvHのデザインとテスト

--- Design.py
+++ Design.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 class GUIGameDesign:
     def __init__(self):
@@ -9,7 +10,7 @@ class GUIGameDesign:
         # メッセージ作成
         self.frame_message = tk.Frame(self.root)
         self.frame_message.pack(side="top")
-        self.label = tk.Label(self.frame_message, text="", font=("Arial", 10))
+        self.label = tk.Label(self.frame_message, text="", font=("游ゴシック Medium", 10))
         self.label.pack(pady=5)
 
         # 盤面作成
@@ -28,3 +29,54 @@ class GUIGameDesign:
                     )
                 c.grid(row=x, column=y)
                 self.canvases[x][y] = c
+
+class GUIPlayerInputDesignForHvH:
+    def __init__(self):
+        # 部品
+        ## root
+        self.root = tk.Tk()
+        self.root.title("プレイヤー設定")
+
+        # 色リスト
+        self.colors = ("白", "黒")
+
+        ## 先攻
+        self.first_frame = tk.Frame(self.root)
+        self.first_label = tk.Label(self.first_frame, text="先攻", font=("游ゴシック Medium", 10))
+        self.first_name_label = tk.Label(self.first_frame, text="名前", font=("游ゴシック Medium", 10))
+        self.first_name_entry = tk.Entry(self.first_frame)
+        self.first_color_label = tk.Label(self.first_frame, text="色", font=("游ゴシック Medium", 10))
+        self.first_v = tk.StringVar()
+        self.first_color_combobox = ttk.Combobox(self.first_frame, height=2, state="readonly", textvariable=self.first_v, values=self.colors, width=2)
+
+        ## 後攻
+        self.second_frame = tk.Frame(self.root)
+        self.second_label = tk.Label(self.second_frame, text="後攻", font=("游ゴシック Medium", 10))
+        self.second_name_label = tk.Label(self.second_frame, text="名前", font=("游ゴシック Medium", 10))
+        self.second_name_entry = tk.Entry(self.second_frame)
+        self.second_color_label = tk.Label(self.second_frame, text="色", font=("游ゴシック Medium", 10))
+        self.second_v = tk.StringVar()
+        self.second_color_combobox = ttk.Combobox(self.second_frame, height=2, state="readonly", textvariable=self.second_v, values=self.colors, width=2)
+        
+        ## スタートボタン
+        self.start_button = tk.Button(self.root, text="スタート", font=("游ゴシック Medium", 10))
+
+        # GUI作成
+        self.first_frame.pack()
+        self.first_label.pack(side=tk.TOP, anchor=tk.W, pady=2)
+        self.first_name_label.pack(side=tk.LEFT, padx=2)
+        self.first_name_entry.pack(side=tk.LEFT, padx=2)
+        self.first_color_label.pack(side=tk.LEFT)
+        self.first_color_combobox.pack(side=tk.LEFT)
+        
+        self.second_frame.pack()
+        self.second_label.pack(side=tk.TOP, anchor=tk.W, pady=2)
+        self.second_name_label.pack(side=tk.LEFT, padx=2)
+        self.second_name_entry.pack(side=tk.LEFT, padx=2)
+        self.second_color_label.pack(side=tk.LEFT)
+        self.second_color_combobox.pack(side=tk.LEFT)
+
+        self.start_button.pack(side=tk.TOP)
+
+        # mainloop
+        self.root.mainloop()
--- Design_test.py
+++ Design_test.py
@@ -1,11 +1,15 @@
-from Design import GUIGameDesign
+from Design import GUIGameDesign, GUIPlayerInputDesignForHvH
 
 class DesignTest:
     def test_gui_game_design(self):
         gui_game_design = GUIGameDesign()
         gui_game_design.root.mainloop()
+    
+    def test_player_input_design(self):
+        GUIPlayerInputDesignForHvH()
 
 
 if __name__ == "__main__":
     design_test = DesignTest()
-    design_test.test_gui_game_design()
+    # design_test.test_gui_game_design()
+    design_test.test_player_input_design()
