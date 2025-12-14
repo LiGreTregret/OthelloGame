cb218f74ffcd3e62f0eb767328e24f68f1073d9e
LiGreT <ligret.regret@gmail.com>
2025-12-15 00:40:06 +0900

    削除ファイル反映

--- PlayerInputDesign_test.py
+++
@@ -1,9 +0,0 @@
-from PlayerInputDesign import GUIModeDesign
-
-class PlayerInputDesignTest:
-    def test_mode_design(self):
-        GUIModeDesign()
-
-if __name__ == "__main__":
-    design_test = PlayerInputDesignTest()
-    design_test.test_mode_design()
