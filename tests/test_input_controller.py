1feab3f3208755f714bd1c62f5535920dc62245a
LiGreT <ligret.regret@gmail.com>
2026-01-21 14:57:57 +0900

    test_input_controllerの構造変更とIssueとの関連付け練習 (テストを作り直そう Fixes #2)

--- tests/test_input_controller.py
+++ tests/test_input_controller.py
@@ -2,31 +2,26 @@ from src.design.game_design import GUIGameDesign
 from src.board.board import Board, BoardOutputToGUI, BoardOutputContext
 from src.controller.input_controller import InputControllerGUI
 
-class TestInputController:
-    def test_on_click(self):
-        gui_game_design = GUIGameDesign()
-        input_controller = InputControllerGUI(gui_game_design)
-        
-        input_controller._on_click(0, 1)
-        assert input_controller.clicked_pos == (0, 1)
-
-        input_controller._on_click(7, 7)
-        assert input_controller.clicked_pos == (7, 7)
+def test_on_click(self):
+    gui_game_design = GUIGameDesign()
+    input_controller = InputControllerGUI(gui_game_design)
     
-    def t_wait_for_click(self):
-        gui_game_design = GUIGameDesign()
+    input_controller._on_click(0, 1)
+    assert input_controller.clicked_pos == (0, 1)
 
-        board = Board()
-        board_output = BoardOutputToGUI("white", "black", gui_game_design)
-        board_output_context = BoardOutputContext()
-        board_output_context.set_method(board_output)
-        board_output_context.execute_output_board(board)
+    input_controller._on_click(7, 7)
+    assert input_controller.clicked_pos == (7, 7)
 
-        input_controller = InputControllerGUI(gui_game_design)
+def test_wait_for_click(self):
+    gui_game_design = GUIGameDesign()
 
-        pos = input_controller.wait_for_click(gui_game_design.root)
-        print(pos)
+    board = Board()
+    board_output = BoardOutputToGUI("white", "black", gui_game_design)
+    board_output_context = BoardOutputContext()
+    board_output_context.set_method(board_output)
+    board_output_context.execute_output_board(board)
 
-if __name__ == "__main__":
-    input_controller_test = TestInputController()
-    input_controller_test.t_wait_for_click()
+    input_controller = InputControllerGUI(gui_game_design)
+
+    pos = input_controller.wait_for_click(gui_game_design.root)
+    print(pos)
