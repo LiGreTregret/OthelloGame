c5541245346434d710e098548dd8c5a27690385a
LiGreT <ligret.regret@gmail.com>
2026-02-01 02:46:35 +0900

    test_player_input削除

--- tests/test_player_input_design.py
+++
@@ -1,9 +0,0 @@
-from src.design.player_input_design import GUIModeDesign
-
-class PlayerInputDesignTest:
-    def test_mode_design(self):
-        GUIModeDesign()
-
-if __name__ == "__main__":
-    design_test = PlayerInputDesignTest()
-    design_test.test_mode_design()
